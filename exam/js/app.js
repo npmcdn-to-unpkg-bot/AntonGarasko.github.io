434bfa5dced71d08a6b3f02046f9eb52735f1e12
Anton Garasko <tonius7570@gmail.com>
2016-07-05 10:24:13 +0300

    added exam

---
+++ exam/js/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.carousel').owlCarousel({
+        loop:true,
+        margin: 10,
+        nav: true,
+        navText: [],
+        responsive:{
+            0:{
+                items:1
+            }
+        }
+    })
+});
