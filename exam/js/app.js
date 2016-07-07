330dddde790047211d329e7b9cd28699d3674409
Anton Garasko <tonius7570@gmail.com>
2016-07-07 18:02:34 +0300

    added components ideas, search, footer

--- exam/js/app.js
+++ exam/js/app.js
@@ -9,5 +9,60 @@ $(document).ready(function(){
                 items:1
             }
         }
-    })
+    });
+
+    // $('.grid').masonry({
+    //     itemSelector: '.grid-item',
+    //     columnWidth: '.grid-sizer',
+    //     percentPosition: true,
+    //     gutter: 20
+    // });
+
+    function grid() {
+        var $grid = $('.grid').imagesLoaded(function () {
+            $grid.masonry({
+                itemSelector: '.grid-item',
+                columnWidth: '.grid-sizer',
+                percentPosition: true,
+                gutter: 20
+            });
+        })
+    }
+
+    grid();
+
+
+
+    // function search() {
+    //     // $('.ideas').find('div').remove();
+    //     // $('img').remove();
+    //     var $searchKey = $('.search').val();
+    //
+    //     $.ajax({
+    //         url: 'https://pixabay.com/api/?key=2874337-2f4149c7655f3148f045862c0&q=' + $searchKey + '&image_type=photo',
+    //         dataType: 'jsonp',
+    //         success: function (data) {
+    //             var results = data.hits;
+    //             for (var i = 0; i < results.length; i++) {
+    //                 $('.grid-item').append('<img src="' + results[i].webformatURL + '">');
+    //             }
+    //             console.log(data);
+    //             // var $html = $('#container').html();
+    //             // var $content = tmpl($html, data);
+    //             // $('.ideas').append($content);
+    //             grid();
+    //         },
+    //         error: function () {
+    //             alert('Error!');
+    //         }
+    //     });
+    // }
+
+    search();
+    //
+    // $('.search__button').on('click', function (e) {
+    //     e.preventDefault();
+    //     search();
+    //     $('.search__field').val('');
+    // })
 });
